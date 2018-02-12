3581df44e68745b577924c6a3dfb0598bb8bb68d
Raa'Shaun Hunter <RaaShaun.Hunter@Gmail.com>
2018-02-12 15:35:32 -0500

    Moved Encoders and Controllers
    In this commit I added a Logitech F310Controller class based off of the WPILib XboxController class and I moved the encoders into the Chassis subsystem to match how they're connected to the robot.

--- src/org/firstinspires/frc/team322/commands/Brakes.java
+++ src/org/firstinspires/frc/team322/commands/Brakes.java
@@ -11,6 +11,8 @@
 
 package org.firstinspires.frc.team322.commands;
 import edu.wpi.first.wpilibj.command.Command;
+
+import org.firstinspires.frc.team322.OI;
 import org.firstinspires.frc.team322.Robot;
 
 /**
@@ -43,22 +45,25 @@ public class Brakes extends Command {
     // Called repeatedly when this Command is scheduled to run
     @Override
     protected void execute() {
+    	Robot.chassis.brakesOn();
     }
 
     // Make this return true when this Command no longer needs to run execute()
     @Override
     protected boolean isFinished() {
-        return false;
+    	Robot.chassis.brakesOff();
+    	return false;
     }
 
     // Called once after isFinished returns true
     @Override
-    protected void end() {
+    protected void end() {    	
     }
 
     // Called when another command which requires one or more of the same
     // subsystems is scheduled to run
     @Override
     protected void interrupted() {
+    	this.isFinished();
     }
 }
